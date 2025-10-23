b74a113fb89191aa8c15c31008cd5d6aec502c2c
ruan-narici <ruannarici@hotmail.com>
2025-10-22 23:48:02 -0300

    feat: ADD V0014__insert_enderecos

---
+++ scripts/V0014__insert_enderecos.sql
@@ -0,0 +1,91 @@
+INSERT INTO
+  tbl_endereco (
+    nr_logradouro,
+    ds_logradouro,
+    nm_bairro,
+    nm_cidade,
+    nm_estado,
+    nm_pais
+  )
+VALUES
+  (
+    123,
+    'Rua das Flores',
+    'Jardim Primavera',
+    'São Paulo',
+    'São Paulo',
+    'Brasil'
+  ),
+  (
+    1500,
+    'Avenida Paulista',
+    'Bela Vista',
+    'São Paulo',
+    'São Paulo',
+    'Brasil'
+  ),
+  (
+    45,
+    'Rua Sete de Setembro',
+    'Centro',
+    'Rio de Janeiro',
+    'Rio de Janeiro',
+    'Brasil'
+  ),
+  (
+    300,
+    'Avenida Boa Viagem',
+    'Pina',
+    'Recife',
+    'Pernambuco',
+    'Brasil'
+  ),
+  (
+    789,
+    'Rua da Moeda',
+    'Bairro do Recife',
+    'Recife',
+    'Pernambuco',
+    'Brasil'
+  ),
+  (
+    10,
+    'Praça da Sé',
+    'Sé',
+    'São Paulo',
+    'São Paulo',
+    'Brasil'
+  ),
+  (
+    2000,
+    'Avenida Afonso Pena',
+    'Funcionários',
+    'Belo Horizonte',
+    'Minas Gerais',
+    'Brasil'
+  ),
+  (
+    500,
+    'Rua Gonçalves Dias',
+    'Lourdes',
+    'Belo Horizonte',
+    'Minas Gerais',
+    'Brasil'
+  ),
+  (
+    1,
+    'Eixo Monumental',
+    'Praça dos Três Poderes',
+    'Brasília',
+    'Distrito Federal',
+    'Brasil'
+  ),
+  (
+    56,
+    'Rua 24 Horas',
+    'Centro Cívico',
+    'Curitiba',
+    'Paraná',
+    'Brasil'
+  )
+;
