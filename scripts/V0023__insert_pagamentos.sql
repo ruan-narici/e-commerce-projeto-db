160893920704d997bf47f0fb1cbbdebc1045cc92
ruan-narici <ruannarici@hotmail.com>
2025-10-23 00:14:02 -0300

    feat: ADD V0023__insert_pagamentos

---
+++ scripts/V0023__insert_pagamentos.sql
@@ -0,0 +1,24 @@
+INSERT INTO
+  tbl_pagamento (ds_tipo_pagamento, vl_pagamento, id_cliente)
+VALUES
+  ('credito', 2599.90, 1),
+  ('debito', 120.50, 3),
+  ('boleto', 398.00, 2),
+  ('credito', 89.90, 4),
+  ('credito', 450.00, 5),
+  ('boleto', 7500.00, 2),
+  ('debito', 199.00, 1),
+  ('credito', 99.90, 3),
+  ('boleto', 210.00, 5),
+  ('credito', 899.90, 4),
+  ('debito', 799.00, 2),
+  ('credito', 350.00, 1),
+  ('boleto', 299.99, 5),
+  ('credito', 1100.00, 3),
+  ('debito', 49.90, 4),
+  ('boleto', 1500.00, 2),
+  ('credito', 1200.00, 1),
+  ('credito', 4500.00, 1),
+  ('boleto', 89.00, 3),
+  ('debito', 75.50, 5)
+;
