5adb52e664948e6d1c63c44e1d876055d9aefa23
ruan-narici <ruannarici@hotmail.com>
2025-10-23 00:07:20 -0300

    feat: ADD V0020__insert_vendedores

---
+++ scripts/V0020__insert_vendedores.sql
@@ -0,0 +1,14 @@
+INSERT INTO
+  tbl_vendedor (ds_razao_social, id_endereco)
+VALUES
+  ('Vendas Rápidas Ltda', 6),
+  ('Representações Comerciais Silva', 8),
+  ('Atacado Vence Tudo', 9),
+  ('Distribuidor Expresso ME', 10),
+  ('Soluções em Vendas S.A.', 11),
+  ('Parceiros de Negócios Eireli', 12),
+  ('Varejista Central', 13),
+  ('Comércio de Importados Ltda', 14),
+  ('Vendedor Autônomo SA', 15),
+  ('Equipe de Vendas Premium', 16)
+;
