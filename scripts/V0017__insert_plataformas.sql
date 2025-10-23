24530055b7006685b5f673db4bf61c9b27734b87
ruan-narici <ruannarici@hotmail.com>
2025-10-22 23:56:44 -0300

    feat: ADD V0017__insert_plataformas

---
+++ scripts/V0017__insert_plataformas.sql
@@ -0,0 +1,9 @@
+INSERT INTO
+  tbl_plataforma (ds_razao_social)
+VALUES
+  ('Comércio Digital Rápido S.A.'),
+  ('Marketplace Brasil Ltda.'),
+  ('Varejo Online Global Eireli'),
+  ('Plataforma de Vendas Conectadas ME'),
+  ('Soluções de E-commerce Integrado S.A.')
+;
