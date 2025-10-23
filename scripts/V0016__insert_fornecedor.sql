1d4b6f8b7669dcaf04d0429ac286cc5e29818acd
ruan-narici <ruannarici@hotmail.com>
2025-10-22 23:55:38 -0300

    feat: ADD V0016__insert_fornecedor

---
+++ scripts/V0016__insert_fornecedor.sql
@@ -0,0 +1,10 @@
+INSERT INTO
+  tbl_fornecedor (ds_razao_social, id_endereco)
+VALUES
+  (
+    'Distribuidora de Componentes Eletrônicos Ltda',
+    2
+  ),
+  ('Software House S.A.', 4),
+  ('Logística Express Transportes', 5)
+;
