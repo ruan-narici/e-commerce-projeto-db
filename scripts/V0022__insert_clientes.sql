697392f4dd485b7909062d9b3f3090fc3901b9c3
ruan-narici <ruannarici@hotmail.com>
2025-10-23 00:12:02 -0300

    feat: ADD V0022__insert_clientes

---
+++ scripts/V0022__insert_clientes.sql
@@ -0,0 +1,17 @@
+INSERT INTO
+  tbl_cliente (nm_cliente, cd_cpf_cnpj, id_endereco)
+VALUES
+  ('Ana Luiza Silva', '111.222.333-44', 17),
+  (
+    'Empresa de TI Soluções Ltda',
+    '12.345.678/0001-99',
+    18
+  ),
+  ('Marcos Pereira', '444.555.666-77', 19),
+  (
+    'Restaurante Sabor do Nordeste',
+    '98.765.432/0001-11',
+    20
+  ),
+  ('Juliana Costa', '888.999.000-11', 21)
+;
