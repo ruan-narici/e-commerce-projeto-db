5fafe108430e0692f8f826d6a637e34c959a8832
ruan-narici <ruannarici@hotmail.com>
2025-10-22 23:59:45 -0300

    feat: ADD V0018__insert_produtos

---
+++ scripts/V0018__insert_produtos.sql
@@ -0,0 +1,120 @@
+INSERT INTO
+  tbl_produto (
+    nm_categoria,
+    ds_produto,
+    vl_produto,
+    id_plataforma,
+    id_fornecedor
+  )
+VALUES
+  (
+    'Eletrônicos',
+    'Smartphone 5G 128GB',
+    2500.00,
+    1,
+    1
+  ),
+  (
+    'Eletrônicos',
+    'Notebook Gamer RTX 4060',
+    7500.00,
+    1,
+    1
+  ),
+  (
+    'Software',
+    'Licença Antivírus 1 Ano',
+    99.90,
+    2,
+    2
+  ),
+  ('Livros', 'Livro de SQL Avançado', 120.50, 3, 3),
+  ('Casa', 'Cafeteira Elétrica', 199.00, 3, 3),
+  (
+    'Eletrônicos',
+    'Fone de Ouvido Bluetooth',
+    350.00,
+    1,
+    1
+  ),
+  (
+    'Software',
+    'Sistema de Gestão (SaaS Mensal)',
+    299.99,
+    2,
+    2
+  ),
+  (
+    'Escritório',
+    'Cadeira de Escritório Ergonômica',
+    899.90,
+    4,
+    3
+  ),
+  ('Eletrônicos', 'Mouse sem Fio', 89.00, 5, 1),
+  (
+    'Software',
+    'Licença Editor de Imagem',
+    450.00,
+    2,
+    2
+  ),
+  ('Roupas', 'Camiseta Básica Algodão', 49.90, 3, 3),
+  ('Eletrônicos', 'Smartwatch GPS', 1200.00, 1, 1),
+  (
+    'Móveis',
+    'Mesa de Jantar 6 Lugares',
+    1500.00,
+    4,
+    3
+  ),
+  (
+    'Games',
+    'Console de Videogame Última Geração',
+    4500.00,
+    1,
+    1
+  ),
+  (
+    'Software',
+    'Plano Anual Ferramenta de Design',
+    1100.00,
+    2,
+    2
+  ),
+  (
+    'Alimentos',
+    'Caixa de Barras de Proteína (12 un)',
+    89.90,
+    5,
+    3
+  ),
+  (
+    'Eletrônicos',
+    'Teclado Mecânico RGB',
+    450.00,
+    1,
+    1
+  ),
+  (
+    'Ferramentas',
+    'Kit Chave de Fenda (10 peças)',
+    75.50,
+    4,
+    3
+  ),
+  (
+    'Saúde',
+    'Monitor de Pressão Arterial Digital',
+    210.00,
+    5,
+    1
+  ),
+  (
+    'Software',
+    'Licença Sistema Operacional Pro',
+    799.00,
+    2,
+    2
+  )
+;
